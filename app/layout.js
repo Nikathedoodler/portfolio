ab15610f9dc2d6ab38622a43222fa61dae29fc01
Nikathedoodler <kakulia.nika@gmai.com>
2025-07-04 13:57:18 +0400

    add vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,31 @@
-import { Outfit, Ovo } from "next/font/google";
-import "./globals.css";
+import { Outfit, Ovo } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/next';
+import './globals.css';
 
 const outfit = Outfit({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
+    subsets: ['latin'],
+    weight: ['400', '500', '600', '700'],
 });
 
 const ovo = Ovo({
-  subsets: ["latin"],
-  weight: ["400"],
+    subsets: ['latin'],
+    weight: ['400'],
 });
 
 export const metadata = {
-  title: "Portfolio - Nika Kakulia",
-  description: "",
+    title: 'Portfolio - Nika Kakulia',
+    description: '',
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en" className="scroll-smooth dark">
-      <body
-        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" className="scroll-smooth dark">
+            <body
+                className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
+            >
+                {children}
+                <Analytics />
+            </body>
+        </html>
+    );
 }
